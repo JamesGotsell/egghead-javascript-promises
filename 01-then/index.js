ed871b389a261ae00b01e50c6a4b68a518905457
James Gotsell <j.e.gotsell@gmail.com>
2019-03-01 23:43:02 +0000

    updated tings

--- 01-then/index.js
+++ 01-then/index.js
@@ -1,5 +1,10 @@
 const API_URL = "https://starwars.egghead.training/";
+const rickNMorty = "https://rickandmortyapi.com/api/"
 const output = document.getElementById("output");
+const spinner = document.getElementById("spinner");
+const output2 = document.getElementById("output2");
+const spinner2 = document.getElementById("spinner-for-ricknmorty");
+
 
 function getFilmTitles(films) {
   return films
@@ -9,10 +14,51 @@ function getFilmTitles(films) {
     .join("\n");
 }
 
-output.innerText = "Loading ...";
+function displayCharacters(characters) {
+  return characters.map( char => `
+    ${char.name} ${char.species}. ${char.type} 
+  ` ).join('\n')
+}
 
+output2.innerText = "Loading ...";
 fetch(API_URL + "films")
-  .then(response => response.json())
-  .then(films => {
-    output.innerText = getFilmTitles(films);
-  });
+  .then(response => {
+    if(!response.ok){
+      // throw Error("Unsuccessful response")
+      return Promise.reject(
+        new Error("unsuccessful response")
+      )
+    }
+    return response.json().then( films => {
+      output.innerText = getFilmTitles(films)
+    })
+  })
+  .catch(error => {
+    console.warn(error)
+    output.innerText = ':('
+   
+  })
+  .finally(() => {
+    // finally method used to for clean up logic
+    spinner.remove()
+  })
+ 
+
+fetch(rickNMorty + "character/" )
+    .then(response => {
+      if(!response.ok){
+        throw Error("Unsuccessful response")
+      }
+      return response.json().then(data => {
+        let chars = data.results
+        output2.innerText = displayCharacters(chars)
+      })
+    })
+    .catch(error => {
+      console.warn(error)
+      output2.innerText = ":("
+    })
+    .finally(() => {
+      spinner2.remove()
+    })
+  
